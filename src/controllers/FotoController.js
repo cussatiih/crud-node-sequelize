1e8aa4d9e7c7aba5559fab26b7f2099224dbc853
cussatiih <cussatiih@gmail.com>
2022-03-18 16:33:17 -0300

    Upload and save picture

--- src/controllers/FotoController.js
+++ src/controllers/FotoController.js
@@ -1,6 +1,29 @@
+import multer from 'multer';
+import multerConfig from '../config/multer';
+import Foto from '../models/Foto';
+
+const upload = multer(multerConfig).single('foto');
+
 class FotoController {
-  async store(req, res) {
-    return res.status(200).json(req.file);
+  store(req, res) {
+    return upload(req, res, async (error) => {
+      if (error) {
+        return res.status(400).json({
+          errors: [error.code],
+        });
+      }
+      try {
+        const { originalname, filename } = req.file;
+        const { aluno_id } = req.body;
+        const foto = await Foto.create({ aluno_id, originalname, filename });
+
+        return res.status(200).json(foto);
+      } catch (e) {
+        return res.status(400).json({
+          errors: ['Aluno não existe.'],
+        });
+      }
+    });
   }
 }
 
--- src/routes/fotoRoutes.js
+++ src/routes/fotoRoutes.js
@@ -1,12 +1,9 @@
 import { Router } from 'express';
-import multer from 'multer';
-import multerConfig from '../config/multer';
 import fotoController from '../controllers/FotoController';
-
-const upload = multer(multerConfig);
+import loginRequired from '../middlewares/loginRequired';
 
 const router = new Router();
 
-router.post('/', upload.single('foto'), fotoController.store);
+router.post('/', loginRequired, fotoController.store);
 
 export default router;
